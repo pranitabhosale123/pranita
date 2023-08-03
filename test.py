79e799a1d9a5887d9b0a0c9529cb105ebd991550
pranitabhosale123 <141301875+pranitabhosale123@users.noreply.github.com>
2023-08-02 23:31:04 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+import webapp2
+
+class Mainpage(webapp2.RequestHandler):
+
+def get(self):
+
+self.response.write("Hello word")
+
+app=webapp2.WSGIApplication([('/',Mainpage),
+
+],
+
+debug=True)
